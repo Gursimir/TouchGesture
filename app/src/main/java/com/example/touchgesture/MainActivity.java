abdaa7dbd18b80e49b25200eda8fccb77c955125
Gursimir <kgursimran648@gmail.com>
2020-11-14 23:21:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/touchgesture/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.touchgesture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create an object of the Android_Gesture_Detector  Class
+        Android_Gesture_Detector  android_gesture_detector  =  new Android_Gesture_Detector();
+        // Create a GestureDetector
+        GestureDetector  mGestureDetector = new GestureDetector(this, android_gesture_detector);
+    }
+}
+class Android_Gesture_Detector implements GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener {
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+}
