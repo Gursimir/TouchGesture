0788e52e637108915d1e817984e166f5912d0157
Gursimir <kgursimran648@gmail.com>
2020-11-15 13:45:23 +0530

    Commit

---
+++ app/src/main/java/com/example/touchgesture/GestureDetector.java
@@ -0,0 +1,8 @@
+package com.example.touchgesture;
+
+public class GestureDetector extends MainActivity {
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        
+    }
+}
--- app/src/main/java/com/example/touchgesture/MainActivity.java
+++ app/src/main/java/com/example/touchgesture/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.touchgesture;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    private GestureDetector mGestureDetector;
+    private ImageView imageView;
+    private float scale = 1f;
+     ScaleGestureDetector detector;
+    Android_Gesture_Detector  android_gesture_detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,56 +23,137 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Create an object of the Android_Gesture_Detector  Class
-        Android_Gesture_Detector  android_gesture_detector  =  new Android_Gesture_Detector();
+        android_gesture_detector  = new Android_Gesture_Detector();
         // Create a GestureDetector
-        GestureDetector  mGestureDetector = new GestureDetector(this, android_gesture_detector);
-    }
-}
-class Android_Gesture_Detector implements GestureDetector.OnGestureListener,
-        GestureDetector.OnDoubleTapListener {
+        mGestureDetector = new GestureDetector(this, android_gesture_detector);
 
-    @Override
-    public boolean onSingleTapConfirmed(MotionEvent e) {
-        return false;
+        imageView=findViewById(R.id.imageView);
+        detector = new ScaleGestureDetector(this,new ScaleListener());
     }
 
     @Override
-    public boolean onDoubleTap(MotionEvent e) {
-        return false;
+    public boolean onTouchEvent(MotionEvent event) {
+        detector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 
-    @Override
-    public boolean onDoubleTapEvent(MotionEvent e) {
-        return false;
+    static class Android_Gesture_Detector implements GestureDetector.OnGestureListener,
+            GestureDetector.OnDoubleTapListener {
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            Log.d("Gesture ", " onDown");
+            return true;
+        }
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            Log.d("Gesture ", " onSingleTapConfirmed");
+            return true;
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            Log.d("Gesture ", " onSingleTapUp");
+            return true;
+        }
+
+        @Override
+        public void onShowPress(MotionEvent e) {
+            Log.d("Gesture ", " onShowPress");
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            Log.d("Gesture ", " onDoubleTap");
+            return true;
+        }
+
+        @Override
+        public boolean onDoubleTapEvent(MotionEvent e) {
+            Log.d("Gesture ", " onDoubleTapEvent");
+            return true;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            Log.d("Gesture ", " onLongPress");
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+
+            Log.d("Gesture ", " onScroll");
+            if (e1.getY() < e2.getY()){
+                Log.d("Gesture ", " Scroll Down");
+            }
+            if(e1.getY() > e2.getY()){
+                Log.d("Gesture ", " Scroll Up");
+            }
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if (e1.getX() < e2.getX()) {
+                Log.d("Gesture ", "Left to Right swipe: "+ e1.getX() + " - " + e2.getX());
+                Log.d("Speed ", velocityX + " pixels/second");
+            }
+            if (e1.getX() > e2.getX()) {
+                Log.d("Gesture ", "Right to Left swipe: "+ e1.getX() + " - " + e2.getX());
+                Log.d("Speed ", velocityX + " pixels/second");
+            }
+            if (e1.getY() < e2.getY()) {
+                Log.d("Gesture ", "Up to Down swipe: " + e1.getX() + " - " + e2.getX());
+                Log.d("Speed ", velocityY + " pixels/second");
+            }
+            if (e1.getY() > e2.getY()) {
+                Log.d("Gesture ", "Down to Up swipe: " + e1.getX() + " - " + e2.getX());
+                Log.d("Speed ", velocityY + " pixels/second");
+            }
+            return true;
+
+        }
     }
 
-    @Override
-    public boolean onDown(MotionEvent e) {
-        return false;
-    }
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
 
-    @Override
-    public void onShowPress(MotionEvent e) {
 
-    }
+        float onScaleBegin = 0;
+        float onScaleEnd = 0;
 
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            scale *= detector.getScaleFactor();
+            imageView.setScaleX(scale);
+            imageView.setScaleY(scale);
+            return true;
+        }
 
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        return false;
-    }
+        @Override
+        public boolean onScaleBegin(ScaleGestureDetector detector) {
 
-    @Override
-    public void onLongPress(MotionEvent e) {
+            Toast.makeText(getApplicationContext(),"Scale Begin" , Toast.LENGTH_SHORT).show();
+            onScaleBegin = scale;
 
-    }
+            return true;
+        }
 
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        return false;
+        @Override
+        public void onScaleEnd(ScaleGestureDetector detector) {
+
+            Toast.makeText(getApplicationContext(),"Scale Ended",Toast.LENGTH_SHORT).show();
+            onScaleEnd = scale;
+
+            if (onScaleEnd > onScaleBegin){
+                Toast.makeText(getApplicationContext(),"Scaled Up by a factor of  " + onScaleEnd / onScaleBegin, Toast.LENGTH_SHORT  ).show();
+            }
+
+            if (onScaleEnd < onScaleBegin){
+                Toast.makeText(getApplicationContext(),"Scaled Down by a factor of  " + onScaleBegin / onScaleEnd, Toast.LENGTH_SHORT  ).show();
+            }
+
+            super.onScaleEnd(detector);
+        }
     }
 }
